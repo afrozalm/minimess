3fd4843494aec730d6e7c82d446762509719748b
afrozalm <alam.afroz011@gnail.com>
2021-04-25 19:36:12 +0530

    added memeber functions to handle connections in a topic; converted subscribers from list to map

--- domain/topic/topic.go
+++ domain/topic/topic.go
@@ -6,11 +6,9 @@ import (
 	"github.com/gorilla/websocket"
 )
 
-// imports
-
 type Topic struct {
-	Name        string
-	Subscribers []*websocket.Conn
+	Name          string
+	SubscriberMap map[string]*websocket.Conn
 }
 
 var ErrUnknown = errors.New("unknown topic")
@@ -23,4 +21,29 @@ type Repository interface {
 	Get(string) (*Topic, error)
 	// Add adds a topic to the repository of topics
 	Add(*Topic) error
+}
+
+func (t *Topic) AddUserConnectionById(uid string, conn *websocket.Conn) {
+	t.SubscriberMap[uid] = conn
+}
+
+func (t *Topic) RemoveUserConnectionById(uid string) {
+	if _, ok := t.SubscriberMap[uid]; ok {
+		delete(t.SubscriberMap, uid)
+	}
+}
+
+func (t *Topic) GetUserConnectionById(uid string) *websocket.Conn {
+	if conn, ok := t.SubscriberMap[uid]; ok {
+		return conn
+	}
+	return nil
+}
+
+func (t *Topic) GetAllConnections() []*websocket.Conn {
+	res := make([]*websocket.Conn, 5)
+	for _, conn := range t.SubscriberMap {
+		res = append(res, conn)
+	}
+	return res
 }
