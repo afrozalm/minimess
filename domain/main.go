055fab242b06ff1f158cfa493d76deab96e8b578
afrozalm <alam.afroz011@gnail.com>
2021-05-06 14:32:10 +0530

    moved run from server to main

--- domain/main.go
+++ domain/main.go
@@ -2,13 +2,26 @@ package main
 
 import (
 	"flag"
+	"log"
+	"net/http"
 
+	"github.com/afrozalm/minimess/domain/connHandler"
 	"github.com/afrozalm/minimess/domain/server"
 )
 
 func main() {
 	port := flag.String("port", "8080", "http service address")
 	flag.Parse()
-	s := server.Create()
-	s.Run("127.0.0.1", *port)
+	s := server.NewServer()
+	addr := "127.0.0.1:" + *port
+	run(s, addr)
+}
+
+func run(s *server.Server, addr string) {
+	http.HandleFunc("/ws", func(w http.ResponseWriter, r *http.Request) {
+		connHandler.ServeWSConn(s, w, r)
+	})
+	if err := http.ListenAndServe(addr, nil); err != nil {
+		log.Fatal("ListenAndServe failed with:", err)
+	}
 }
