2ef6162cbbe21364a84d9a8e11630e6b4dcba060
afrozalm <alam.afroz011@gnail.com>
2021-05-06 14:06:55 +0530

    added a server to run in main

--- domain/main.go
+++ domain/main.go
@@ -1,1 +1,14 @@
 package main
+
+import (
+	"flag"
+
+	"github.com/afrozalm/minimess/domain/server"
+)
+
+func main() {
+	port := flag.String("port", "8080", "http service address")
+	flag.Parse()
+	s := server.Create()
+	s.Run("127.0.0.1", *port)
+}
