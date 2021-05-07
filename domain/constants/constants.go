2ce6fcce7926054a72536b1e3385fe7903a6d171
afrozalm <alam.afroz011@gnail.com>
2021-05-07 16:30:19 +0530

    added constants for all constant in the project

---
+++ domain/constants/constants.go
@@ -0,0 +1,23 @@
+package constants
+
+import (
+	"time"
+)
+
+const (
+	// timeouts in seconds
+	PongTimeout  = 60 * time.Second
+	ReadTimeout  = PongTimeout
+	PingTimeout  = (PongTimeout * 4) / 5
+	WriteTimeout = 10 * time.Second
+
+	// buffer limits
+	MaxMessageSize = 512
+
+	// message constants
+	// Types
+	USER        = "user"
+	SUBSCRIBE   = "sub"
+	UNSUBSCRIBE = "unsub"
+	CHAT        = "chat"
+)
