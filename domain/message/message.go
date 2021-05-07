c37857f79adbbc433da05a0c29707be00945a852
afrozalm <alam.afroz011@gnail.com>
2021-05-07 16:29:30 +0530

    updated message struct to be more universal. Will try thrift later

---
+++ domain/message/message.go
@@ -0,0 +1,29 @@
+package message
+
+import (
+	"bytes"
+	"encoding/json"
+)
+
+var (
+	newline = []byte{'\n'}
+	space   = []byte{' '}
+)
+
+type Message struct {
+	Type  string
+	Uid   string
+	Topic string
+	Text  string
+}
+
+func DecodeMessage(payload []byte) (*Message, error) {
+	payload = bytes.TrimSpace(bytes.Replace(payload, newline, space, -1))
+	m := &Message{}
+	err := json.Unmarshal(payload, m)
+	return m, err
+}
+
+func (m *Message) EncodeMessage() ([]byte, error) {
+	return json.Marshal(m)
+}
