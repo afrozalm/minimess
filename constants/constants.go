0c9edc02d72c5b3bae520e6599b1387a1b317c6d
afrozalm <alam.afroz011@gnail.com>
2021-07-09 11:08:46 +0530

    added ports for cassandra, frontend and fanout

--- constants/constants.go
+++ constants/constants.go
@@ -28,9 +28,22 @@ const (
 	// kafka
 	BootstrapServers = "localhost:9091,localhost:9092,localhost:9093"
 	MessageTopic     = "message-log"
+
+	// Broadcaster
+	BroadcasterCG = "BROADCASTER-CG"
 )
 
 var (
 	// kafka
 	Brokers = []string{"localhost:9091", "localhost:9092", "localhost:9093"}
+
+	// cassandra
+	CassandraHosts = []string{"localhost:9042"}
+
+	// fanout
+	FanoutBasePort = 11000
+
+	// frontend
+	FrontendClientBasePort = 12000
+	FrontendGRPCBasePort   = 13000
 )
