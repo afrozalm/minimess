2a79601e94537ea63348b36dc7906b7db5069777
afrozalm <alam.afroz011@gnail.com>
2021-07-11 15:48:17 +0530

    added some debug statements

--- server/fanout/storage/cassandrastorage.go
+++ server/fanout/storage/cassandrastorage.go
@@ -5,6 +5,7 @@ import (
 
 	"github.com/afrozalm/minimess/constants"
 	"github.com/gocql/gocql"
+	log "github.com/sirupsen/logrus"
 )
 
 type CassandraStorage struct {
@@ -31,6 +32,7 @@ func (s *CassandraStorage) Close() {
 }
 
 func (s *CassandraStorage) AddHostTopic(host, topic string) error {
+	log.Tracef("adding hosttopic '%s' '%s'", host, topic)
 	if err := s.session.Query(`INSERT INTO topic_host (topic, host) VALUES (?, ?) USING TTL 600`,
 		topic, host).WithContext(s.ctx).Exec(); err != nil {
 		return err
@@ -39,6 +41,7 @@ func (s *CassandraStorage) AddHostTopic(host, topic string) error {
 }
 
 func (s *CassandraStorage) RemoveHostTopic(host, topic string) error {
+	log.Tracef("removing hosttopic '%s' '%s'", host, topic)
 	if err := s.session.Query(`DELETE FROM topic_host WHERE topic=? and host=?`,
 		topic, host).WithContext(s.ctx).Exec(); err != nil {
 		return err
@@ -47,6 +50,7 @@ func (s *CassandraStorage) RemoveHostTopic(host, topic string) error {
 }
 
 func (s *CassandraStorage) GetHostsForTopic(topic string) []string {
+	log.Tracef("get all hosts for topic '%s'", topic)
 	scanner := s.session.Query(`SELECT host FROM topic_host WHERE topic=?`, topic).
 		WithContext(s.ctx).
 		Iter().
@@ -64,6 +68,7 @@ func (s *CassandraStorage) GetHostsForTopic(topic string) []string {
 }
 
 func (s *CassandraStorage) GetTopicsForHost(host string) []string {
+	log.Tracef("get all topics for host '%s'", host)
 	scanner := s.session.Query(`SELECT topic FROM topic_host WHERE host=?`, host).
 		WithContext(s.ctx).
 		Iter().
